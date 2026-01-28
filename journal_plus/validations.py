4804d8e2cdd1afdb257fc0e9fd4a49779526b38b
ramdani <xhijack@gmail.com>
2026-01-28 22:18:21 +0700

    fixing function

--- journal_plus/validations.py
+++ journal_plus/validations.py
@@ -4,7 +4,7 @@ from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
 
 import frappe
 
-def validate_mandatory_dimensions(self):
+def validate_mandatory_dimensions():
     dimensions = get_accounting_dimensions()
     for dim in dimensions:
         mandatory = frappe.db.get_value(
@@ -12,5 +12,5 @@ def validate_mandatory_dimensions(self):
             {"parent": dim, "mandatory_for_pl": 1},
             "name",
         )
-        if mandatory and not getattr(self, dim, None):
+        if mandatory and not getattr(dim, None):
             frappe.throw(_("{0} is mandatory for Profit and Loss").format(frappe.bold(dim)))
